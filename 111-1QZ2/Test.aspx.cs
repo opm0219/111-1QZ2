c9949b500edf4f0cda3532f0ad165bdff5a2ea63
opm0219 <opm0219@gmail.com>
2022-10-17 14:07:32 +0800

    124242

---
+++ 111-1QZ2/Test.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1QZ2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lb_Msg.Text = "Page_Load";
+        }
+
+        protected System.Void btn_Submit_Click(System.Object sender, System.EventArgs e)
+        {
+            lb_Msg.Text = "btn_Event";
+        }
+    }
+}
